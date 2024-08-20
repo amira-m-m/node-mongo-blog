751c5d48fbc9faaa4b224405de4aa2f78cb73ac1
Amira Mohamed <amm-personal@outlook.com>
2024-08-19 22:47:40 -0400

    Add about page and EJS layouts

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.send("Testing connection");
+    const locals = {
+        title: "Node.js Blog",
+        description: "A simple blog app"
+    }
+    res.render('index', { locals });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
 });
 
 module.exports = router;
