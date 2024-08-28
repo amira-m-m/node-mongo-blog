d8edc39998821e2c198d4d9e91a6c9d34caec134
Amira Mohamed <amm-personal@outlook.com>
2024-08-28 11:07:30 -0400

    Add DB connection and system for blog post routing

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
-router.get('', (req, res) => {
-    const locals = {
-        title: "Node.js Blog",
-        description: "A simple blog app"
+let locals;
+
+router.get('', async (req, res) => {
+
+    try {
+        locals = {
+            title: "Node.js Blog",
+            description: "A simple blog app"
+        };
+        const data = await Post.find();
+        res.render('index', { locals, data });
     }
-    res.render('index', { locals });
+    catch (error) {
+        console.error(error);
+    };
 });
 
 router.get('/about', (req, res) => {
@@ -15,6 +25,22 @@ router.get('/about', (req, res) => {
 
 router.get('/contact', (req, res) => {
     res.render('contact');
+});
+
+router.get('/post/:id', async (req, res) => {
+
+    try {
+        let slug = req.params.id;
+        const data = await Post.findById({ _id: slug });
+        locals = {
+            title: data.title,
+            description: "A post within a simple blog app"
+        };
+        res.render('post', { locals, data, currentRoute: `/posts/${slug}` });
+    }
+    catch (error) {
+        console.error(error);
+    };
 });
 
 module.exports = router;
